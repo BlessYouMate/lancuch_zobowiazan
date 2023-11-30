b50c8c04c55160c10f3a6e2b6f396876aefcf8a1
BlessYouMate <wiktorjck@gmail.com>
2023-11-30 12:26:45 +0100

    moj pierwszy commit

---
+++ Concrete_handlers.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <string>
+#include "Abstract_handler.h"
+
+/**
+* All Concrete Handlers either handle a request or pass it to the next handler
+* in the chain.
+*/
+class MonkeyHandler : public AbstractHandler {
+public:
+	std::string Handle(std::string request) override {
+		if (request == "Banana") {
+			return "Monkey: I'll eat the " + request + ".\n";
+		}
+		else {
+			return AbstractHandler::Handle(request);
+		}
+	}
+};
+class SquirrelHandler : public AbstractHandler {
+public:
+	std::string Handle(std::string request) override {
+		if (request == "Nut") {
+			return "Squirrel: I'll eat the " + request + ".\n";
+		}
+		else {
+			return AbstractHandler::Handle(request);
+		}
+	}
+};
+class DogHandler : public AbstractHandler {
+public:
+	std::string Handle(std::string request) override {
+		if (request == "MeatBall") {
+			return "Dog: I'll eat the " + request + ".\n";
+		}
+		else {
+			return AbstractHandler::Handle(request);
+		}
+	}
+};
